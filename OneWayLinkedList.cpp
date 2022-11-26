485d3073fdeeb19404084c5885f63190a9c80047
Max Base <maxbasecode@gmail.com>
2022-11-26 13:27:56 +0100

    init the project, create .cpp and header file

---
+++ OneWayLinkedList.cpp
@@ -0,0 +1,13 @@
+/*
+ * @Name: One Way Linked List C++
+ * @Author: Max Base
+ * @Date: 2022-11-14
+ * @Repository: https://github.com/basemax/OneWayLinkedListCpp
+ */
+
+#include "OneWayLinkedList.h"
+
+class Node {
+public:
+
+}
---
+++ OneWayLinkedList.h
@@ -0,0 +1,7 @@
+/*
+ * @Name: One Way Linked List C++
+ * @Author: Max Base
+ * @Date: 2022-11-14
+ * @Repository: https://github.com/basemax/OneWayLinkedListCpp
+ */
+
